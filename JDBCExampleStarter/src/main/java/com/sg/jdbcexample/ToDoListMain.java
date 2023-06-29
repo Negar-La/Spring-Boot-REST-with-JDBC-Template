741102c24a54de14df6b416abad5155bbe511a0a
Negar Lashgari <negar.lashgari@gmail.com>
2023-06-29 14:16:31 -0400

    adding implementations

--- JDBCExampleStarter/src/main/java/com/sg/jdbcexample/ToDoListMain.java
+++ JDBCExampleStarter/src/main/java/com/sg/jdbcexample/ToDoListMain.java
@@ -5,7 +5,10 @@
  */
 package com.sg.jdbcexample;
 
-import java.sql.SQLException;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.Scanner;
 
 /**
@@ -16,9 +19,19 @@ public class ToDoListMain {
 
     private static Scanner sc;
 
+    //add in a class level DataSource variable
+    private static DataSource ds;
     public static void main(String[] args) {
 
         sc = new Scanner(System.in);
+
+        try {
+            ds = getDataSource();
+        } catch (SQLException ex) {
+            System.out.println("Error connecting to database");
+            System.out.println(ex.getMessage());
+            System.exit(0);
+        }
 
         do {
             System.out.println("To-Do List");
@@ -58,18 +71,118 @@ public class ToDoListMain {
     }
 
     private static void displayList() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Display All");
+        try ( Connection conn = ds.getConnection()) {
+            Statement stmt = conn.createStatement();   //using a Statement and not a PreparedStatement.
+            ResultSet rs = stmt.executeQuery("SELECT * FROM todo");
+            while (rs.next()) {
+                System.out.printf("%s: %s -- %s -- %s\n",
+                        rs.getString("id"),
+                        rs.getString("todo"),
+                        rs.getString("note"),
+                        rs.getBoolean("finished"));
+            }
+            System.out.println("");
+        }
     }
 
     private static void addItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Add Item");
+        System.out.println("What is the task?");
+        String task = sc.nextLine();
+        System.out.println("Any additional notes?");
+        String note = sc.nextLine();
+
+        try (Connection conn = ds.getConnection()) {
+            String sql = "INSERT INTO todo(todo, note) VALUES(?,?)";
+            PreparedStatement pStmt = conn.prepareCall(sql);
+            pStmt.setString(1, task);
+            pStmt.setString(2, note);
+            pStmt.executeUpdate();
+            System.out.println("Add Complete");
+        }
     }
 
     private static void updateItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Update Item");
+        System.out.println("Which item do you want to update?");
+        String itemId = sc.nextLine();
+        try (Connection conn = ds.getConnection()) {
+            String sql = "SELECT * FROM todo WHERE id = ?";
+            PreparedStatement pStmt = conn.prepareCall(sql);
+            pStmt.setString(1, itemId);
+            ResultSet rs = pStmt.executeQuery();
+            rs.next();
+            ToDo td = new ToDo();  //we use ResultSet to build a ToDo object
+            td.setId(rs.getInt("id"));
+            td.setTodo(rs.getString("todo"));
+            td.setNote(rs.getString("note"));
+            td.setFinished(rs.getBoolean("finished"));
+
+            System.out.println("1. ToDo - " + td.getTodo());
+            System.out.println("2. Note - " + td.getNote());
+            System.out.println("3. Finished - " + td.isFinished());
+            System.out.println("What would you like to change?");
+
+            String choice = sc.nextLine();
+            switch(choice) {
+                case "1":
+                    System.out.println("Enter new ToDo:");
+                    String todo = sc.nextLine();
+                    td.setTodo(todo);
+                    break;
+                case "2":
+                    System.out.println("Enter new Note:");
+                    String note = sc.nextLine();
+                    td.setNote(note);
+                    break;
+                case "3":
+                    System.out.println("Toggling Finished to " + !td.isFinished());
+                    td.setFinished(!td.isFinished());
+                    break;
+                default:
+                    System.out.println("No change made");
+                    return;
+            }
+            String updateSql = "UPDATE todo SET todo = ?, note = ?, finished = ? WHERE id = ?";
+            PreparedStatement updatePStmt = conn.prepareCall(updateSql);
+            updatePStmt.setString(1, td.getTodo());
+            updatePStmt.setString(2, td.getNote());
+            updatePStmt.setBoolean(3, td.isFinished());
+            updatePStmt.setInt(4, td.getId());
+            updatePStmt.executeUpdate();
+            System.out.println("Update Complete");
+        }
     }
 
     private static void removeItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Remove Item");
+        System.out.println("Which item would you like to remove?");
+        String itemId = sc.nextLine();
+
+        try(Connection conn = ds.getConnection()) {
+            String sql = "DELETE FROM todo WHERE id = ?";
+            PreparedStatement pStmt = conn.prepareCall(sql);
+            pStmt.setString(1, itemId);
+            pStmt.executeUpdate();
+            System.out.println("Remove Complete");
+        }
     }
+
+    //Instantiate our DataSource
+    private static DataSource getDataSource() throws SQLException {
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setServerName("localhost");
+        ds.setDatabaseName("todoDB");
+        ds.setUser("root");
+        ds.setPassword("hooman");
+        ds.setServerTimezone("America/Chicago");
+        ds.setUseSSL(false);
+        ds.setAllowPublicKeyRetrieval(true);
+
+        return ds;
+    }
+
 }
+
+
