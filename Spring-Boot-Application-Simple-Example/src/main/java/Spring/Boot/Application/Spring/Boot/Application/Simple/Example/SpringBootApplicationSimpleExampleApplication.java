3b8ea142a35e7e740478f9aaea42edde51480bc8
Negar Lashgari <negar.lashgari@gmail.com>
2023-07-10 13:01:31 -0400

    spring boot simple app

---
+++ Spring-Boot-Application-Simple-Example/src/main/java/Spring/Boot/Application/Spring/Boot/Application/Simple/Example/SpringBootApplicationSimpleExampleApplication.java
@@ -0,0 +1,13 @@
+package Spring.Boot.Application.Spring.Boot.Application.Simple.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationSimpleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationSimpleExampleApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Application-Simple-Example/src/test/java/Spring/Boot/Application/Spring/Boot/Application/Simple/Example/SpringBootApplicationSimpleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot.Application.Spring.Boot.Application.Simple.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationSimpleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
