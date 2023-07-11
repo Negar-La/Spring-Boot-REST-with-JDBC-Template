dacca9b5329ca96499651404030ac0dc878b9ac2
Negar Lashgari <negar.lashgari@gmail.com>
2023-07-11 00:37:22 -0400

    Spring boot jdbc template

---
+++ Spring-Boot-REST-Service-with-JDBC-Template/src/main/java/ToDoAPI/Spring/Boot/REST/Service/JDBC/Template/data/ToDoDao.java
@@ -0,0 +1,19 @@
+package ToDoAPI.Spring.Boot.REST.Service.JDBC.Template.data;
+
+import ToDoAPI.Spring.Boot.REST.Service.JDBC.Template.models.ToDo;
+
+import java.util.List;
+
+public interface ToDoDao {
+    ToDo add(ToDo todo);
+
+    List<ToDo> getAll();
+
+    ToDo findById(int id);
+
+    // true if item exists and is updated
+    boolean update(ToDo todo);
+
+    // true if item exists and is deleted
+    boolean deleteById(int id);
+}
---
+++ Spring-Boot-REST-Service-with-JDBC-Template/src/test/java/ToDoAPI/Spring/Boot/REST/Service/JDBC/Template/AppTests.java
@@ -0,0 +1,13 @@
+package ToDoAPI.Spring.Boot.REST.Service.JDBC.Template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
